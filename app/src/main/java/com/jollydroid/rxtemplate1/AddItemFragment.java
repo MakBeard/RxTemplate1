a665ffdc9633f3f6f9449d7a087749211dbc4f42
Jellydroid <jellybee.droid@gmail.com>
2016-08-01 19:25:25 +0300

    * Activity для ввода значения и даты. * Activity для показа списка.  RecyclerView.  * Drawer для переключения между ними.  * Material Design из Design Support Library
    (нет сортировки, нет работы с базой, нет Rx)

---
+++ app/src/main/java/com/jollydroid/rxtemplate1/AddItemFragment.java
@@ -0,0 +1,15 @@
+package com.jollydroid.rxtemplate1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddItemFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_item_add, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/jollydroid/rxtemplate1/model/VersionModel.java
@@ -0,0 +1,11 @@
+package com.jollydroid.rxtemplate1.model;
+
+
+public class VersionModel {
+    public static final String[] data = {
+            "cupcake", "donut", "eclair", "froyo", "gingerbread",
+            "honeycomb", "ice cream sandwich", "jelly bean", "kit kat",
+            "Lollipop", "marshmallow"
+    };
+
+}
